0f06b6a21f3c8d84ebd07d148cb163b9948fb8bd
lukgowsi <lucaslh@tamu.edu>
2023-04-10 19:42:55 -0500

    login started

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const { Pool } = require("pg");
 const dotenv = require("dotenv").config();
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
+
 // Create express app
 const app = express();
 const port = 3000;
@@ -52,6 +57,47 @@ app.get('/orderquery', (req, res) => {
            
       });
 });
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+    clientID: "947810255577-aggap7cvgjsnk288h2opb13igc811d0s.apps.googleusercontent.com",
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // This function will be called after successful authentication
+    // You can use the "profile" object to get information about the authenticated user
+    done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // This function will be called after successful authentication
+    // Redirect the user to the home page or some other page
+    res.redirect('/');
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
