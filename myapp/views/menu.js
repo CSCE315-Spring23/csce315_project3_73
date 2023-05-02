e81ac8b7d3ee379402933cc96e1b51ee9131842a
tfella <tylerfelton@tamu.edu>
2023-05-02 16:46:46 -0500

    fixes style

--- myapp/views/menu.js
+++ myapp/views/menu.js
@@ -8,12 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.json())
     .then((data) => {
       const temperature = (data.main.temp - 273.15) * 1.8 + 32;
-      const roundedTemp = temperature.toFixed(2);
+      const roundedTemp = temperature.toFixed(0);
       const description = data.weather[0].description;
       const city = data.name;
       const country = data.sys.country;
 
-      weatherDiv.innerHTML = `The weather in ${city}, ${country} is ${roundedTemp}°F and ${description}.`;
+      weatherDiv.innerHTML = `${roundedTemp}°F`;
     })
     .catch((error) => {
       console.log(error);
