6b47710a38e63692a91d2e6b3228a39f26a6af1b
tfella <tylerfelton@tamu.edu>
2023-05-03 22:36:14 -0500

    fix time

--- myapp/views/manager.js
+++ myapp/views/manager.js
@@ -252,10 +252,19 @@ function generateXReport() {
   let salesAmount = 0.0;
   let itemQuantities = new Map();
 
-  let currentDate = new Date();
-  let formattedDate = currentDate.toISOString().slice(0, 19).replace("T", " ");
+  const currentDate = new Date();
 
-  let startDate = currentDate.toISOString().slice(0, 10);
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getDate()).padStart(2, '0');
+
+const hours = String(currentDate.getHours()).padStart(2, '0');
+const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+  const formattedTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+  let startDate = `${year}-${month}-${day}`
 
   try {
     const query = encodeURIComponent(
